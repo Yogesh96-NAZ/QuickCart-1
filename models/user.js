86d3b3434f202a4bd97e6facffcce3f7eb6a91f0
Yogesh96-Naz <you@example.com>
2025-03-26 21:18:14 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,16 @@
+// import { CartIcon } from "@/assets/assets";
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema = new mongoose.Schema({
+    _id:{type: String, required: true},
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    imageUrl: { type: String, required: true, unique: true },
+    cartItems: { type: Object, default: {}
+ },},{ minimize: false}
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
